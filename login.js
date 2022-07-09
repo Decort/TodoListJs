1beedc3a3054d12270b2cfe7df43ae2033300bed
Danco <danielecortesp@gmail.com>
2022-07-08 23:39:03 -0500

    arreglo del boton submit y login

--- login.js
+++ login.js
@@ -1,16 +1,25 @@
-function capture(){
-    var userName = document.getElementById('user').value;
-    var password = document.getElementById('password').value;
-    if (userName !== 'abc') {
-        alert('Nombre incorrecto');
-        document.getElementById('user').focus();
-    } else if (password !== '123') {
-            alert('contraseña incorrecta');
-            document.getElementById('password').focus(); 
-    } else if ( userName === 'abc' && password === '123') {
-        alert('registro correcto!');
-    }
-    }
+let userName;
+let password;
+
+
+
+const loginButton = document.getElementById('login');
+const form = document.getElementById('login-form');
+form.addEventListener('submit', onSubmit);
+
+
+
+
+function onSubmit(event){ 
+    userName = document.getElementById('user').value;
+    password = document.getElementById('password').value;
+    if (userName !== 'abc' || password !== '123') {
+        alert('Credenciales incorrectass');
+        event.preventDefault();
+        return 
+    } 
+    window.location.replace('www.google.com');  
+}
 
 
     
