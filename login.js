7f7f993343155ae696e4b27e3ac4ccae7944efc2
Danco <danielecortesp@gmail.com>
2022-07-13 14:04:37 -0500

    PR comments

--- login.js
+++ login.js
@@ -1,18 +1,16 @@
 let userName;
 let password;
 
-const loginButton = document.getElementById("login");
-const form = document.getElementById("login-form");
-form.addEventListener("submit", onSubmit);
+const loginButton = document.getElementById('login');
+const form = document.getElementById('login-form');
+form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
-  userName = document.getElementById("user").value;
-  password = document.getElementById("password").value;
-  if (userName !== "abc" || password !== "123") {
-    alert("Credenciales incorrectass");
+  userName = document.getElementById('user').value;
+  password = document.getElementById('password').value;
+  if (userName !== 'abc' || password !== '123') {
+    alert('Credenciales incorrectass');
     event.preventDefault();
     return;
   }
-  window.open("https://www.google.com/?hl=es");
 }
-// window.location.replace('www.google.com') el que habiamos puesto
